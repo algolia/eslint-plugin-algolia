ce613c26c292dc2c980b55dd7eb0de50a160b486
Matthieu Dumont <jerska@live.fr>
2015-12-07 11:13:43 +0100

    chore(release): Use the version from the package.json

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,12 +5,10 @@ if test -n "$(git status --porcelain)"; then
   exit
 fi
 
-current=`cat VERSION`
+current=`cat package.json | json version`
 read -p "New version number (current is ${current}): " version
 npm version $version
 npm run build
-rm -f VERSION && echo $version > VERSION
-git commit VERSION --amend -m "v$version"
 git push
 git push --tags
 npm publish
