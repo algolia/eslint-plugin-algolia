46b7a42c87a599cdcb2f0fbd99b66ed3bbdc08ae
Matthieu Dumont <jerska@live.fr>
2015-12-07 11:46:59 +0100

    fix(no-require): Syntax error

--- rules/no-require.js
+++ rules/no-require.js
@@ -1,12 +1,14 @@
 // Rule definition
 module.exports = (context) => {
-  return CallExpression: (node) => {
-    // Only check requires
-    if (node.callee.name !== 'require') return;
+  return {
+      CallExpression: (node) => {
+      // Only check requires
+      if (node.callee.name !== 'require') return;
 
-    context.report({
-      node: node,
-      message: 'Prefer import over require'
-    });
+      context.report({
+        node: node,
+        message: 'Prefer import over require'
+      });
+    }
   };
 };
