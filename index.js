1e131a520782928dfd1692f9c30bb31d7a5f9794
Matthieu Dumont <jerska@live.fr>
2015-12-07 11:54:40 +0100

    chore(index.js): Add missing no-require

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
   rules: {
     'relative-require': require('./rules/relative-require.js'),
-    'force-import-root': require('./rules/force-import-root.js')
+    'force-import-root': require('./rules/force-import-root.js'),
+    'no-require': require('./rules/no-require.js')
   },
   rulesConfig: {
     'relative-require': 0,
     'force-import-root': 0,
+    'no-require': 0
   }
 };
