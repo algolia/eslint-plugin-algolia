2880c76e6d6b2f50c304addd937976400e58bad5
Samuel Bodin <samuel.bodin@algolia.com>
2020-03-05 18:07:39 +0100

    feat: new rule func-style-toplevel

---
+++ rules/__tests__/func-style-toplevel.test.js
@@ -0,0 +1,96 @@
+/**
+ * @fileoverview Tests for func-style-toplevel rule.
+ * @author Samuel Bodin
+ */
+
+// Requirements
+
+const { RuleTester } = require('eslint');
+const path = require('path');
+
+const rule = require('../func-style-toplevel');
+const BABEL_ESLINT = path.join(__dirname, '../../node_modules', 'babel-eslint');
+
+// Tests
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
+  },
+});
+
+ruleTester.run('func-style-toplevel', rule, {
+  valid: [
+    {
+      code: `
+      function foo(){};
+      `,
+    },
+    {
+      code: `
+      {
+        var foo = () => {};
+      }
+      `,
+    },
+    {
+      code: `
+      function foo() {
+        var foo = () => {};
+      };
+      `,
+      parser: BABEL_ESLINT,
+    },
+    {
+      code: `
+      function foo() {
+        function bar() {};
+      };
+      `,
+      parser: BABEL_ESLINT,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `
+        var foo = () => {};
+      `,
+      errors: [
+        {
+          messageId: 'declaration',
+        },
+      ],
+    },
+    {
+      code: `
+        const foo = () => {};
+      `,
+      errors: [
+        {
+          messageId: 'declaration',
+        },
+      ],
+    },
+    {
+      code: 'var foo = function(){};',
+      errors: [
+        {
+          messageId: 'declaration',
+        },
+      ],
+    },
+    {
+      code: 'var foo = function bar(){};',
+      errors: [
+        {
+          messageId: 'declaration',
+        },
+      ],
+    },
+  ],
+});
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-for file in `echo **/__tests__/*-test.js`; do
-  babel-node $file
+files=$(find rules/__tests__/ -type f)
+for file in $files; do
+  node $file
 done
 
 npm run lint
